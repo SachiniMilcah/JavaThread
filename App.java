da6f21f18e19a388f970f14652ffcbb4fff4c176
SachiniMilcah <44254803+SachiniMilcah@users.noreply.github.com>
2020-09-30 11:01:38 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,32 @@
+package mavenpac;
+
+
+public class App {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        System.err.println("Thread Id:"+ Thread.currentThread().getId());
+                        
+                    }
+                }).start();
+     
+                try {
+                    int random = (int)(5000 * Math.random() + 0);
+                    long start = System.currentTimeMillis();
+                
+                    Thread.sleep(random);
+                    
+                    System.out.println("Sleep time = "+(System.currentTimeMillis()-start)+ " "+ "ms");
+
+                    
+                } catch (InterruptedException ex) {
+                    
+                    
+                }
+            }
+        }
+
+
+}
+        
